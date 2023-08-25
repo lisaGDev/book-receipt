9b54b0ad89eb759954f7925a7db350487f7c30bb
Lisa Galva <lisagalva@Lisas-Air.fios-router.home>
2023-08-24 22:56:30 -0400

    add functions in Class Dish

--- dish.py
+++ dish.py
@@ -1,5 +1,5 @@
 import sqlite3
-import flask
+# import flask
 
 
 class Dish:
@@ -29,6 +29,31 @@ class Dish:
         sql_delete_query = "DELETE FROM Dish WHERE id_dish = %s" % id_dish
         self.cur.execute(sql_delete_query)
         self.connection.commit()  # Remember to commit the transaction after executing DELETE.
+
+    def create_tuple_of_ingredients(self, dish_name):
+        """get all the ingredients for the selected dish."""
+        get_id_dish_by_name_res = self.cur.execute("SELECT id_dish FROM Dish WHERE name = '%s'" % dish_name).fetchone()[0]
+        get_composition_by_id_ingredient_res_tuple = tuple(i[0] for i in self.cur.execute("SELECT id_ingredients FROM Composition WHERE id_dish = %s" % get_id_dish_by_name_res).fetchall())
+
+        return get_composition_by_id_ingredient_res_tuple
+
+    def get_ingredients_by_dish(self, dish_name):
+        get_composition_by_id_ingredient_res_tuple = self.create_tuple_of_ingredients(dish_name)
+        get_ingredients_by_id_ingredient_res = self.cur.execute("SELECT * FROM Ingredients WHERE Ingredients.id_ingredient IN %s" % str(get_composition_by_id_ingredient_res_tuple)).fetchall()
+
+        return get_ingredients_by_id_ingredient_res
+
+    def price_ingredients_for_dish(self, dish_name):
+        """get the price of a dish according to its ID."""
+        get_composition_by_id_ingredient_res_tuple = self.create_tuple_of_ingredients(dish_name)
+        get_price_by_dish = self.cur.execute("SELECT SUM(Ingredients.price) FROM Ingredients WHERE Ingredients.id_ingredient IN %s" % str(get_composition_by_id_ingredient_res_tuple)).fetchone()[0]
+
+        return get_price_by_dish
+
+    def order_price(self):
+        """Create a request to sort by the price of the dish."""
+        res = self.cur.execute("SELECT name from Dish ORDER BY price")
+        return res.fetchall()
 
     def end(self):
         """close sqlite3 connection."""
